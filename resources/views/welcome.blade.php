0eede8d48ea44a256e34acb668a4c397763b7ccb
Paps94 <antreas@OneHappyNoob.localdomain>
2018-09-02 12:28:59 +0100

    Fixed typo of the title of the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Baloo</title>
+        <title>Balloo</title>
 
         <!-- Styles -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
