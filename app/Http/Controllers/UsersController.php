bb6464a7e4388607a53c4f84a9d005a32ecef473
Paps94 <antreas@OneHappyNoob.localdomain>
2018-09-02 15:13:30 +0100

    Changes to the way you upload images

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,8 +23,8 @@ class UsersController extends Controller
          $avatar = $request->file('avatar');
          $filename = time() . '.' . $avatar->getClientOriginalExtension();
          $path = public_path('/image/avatars/')."/" . $filename;
+         dd(is_writable($path . $fileName));
          $img = Image::make($avatar)->fit(320, 320)->save($path);
-         echo 1;exit;
          //check if the user is the signed in user
          $user = Auth::user();
          //Save old image to a new variable for deleting purposes
